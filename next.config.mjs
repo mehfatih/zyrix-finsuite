6baa7c95591da5bd6458547764b8f4285d3395d5
mehfatih <meh.fatih77@gmail.com>
2026-04-14 18:33:00 +0300

    fix: use next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["next-intl"],
+  },
+  images: {
+    formats: ["image/webp", "image/avif"],
+  },
+};
+
+export default withNextIntl(nextConfig);
+{\rtf1}
